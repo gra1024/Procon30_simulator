dd6f0247474e430f447a64127ca5362f0751caab
ru314 <lad7a3ded7dwny3@i.softbank.jp>
2019-04-16 16:58:34 +0900

    [add]PointCalculate
    少し

--- src/PointCalculate.cpp
+++ src/PointCalculate.cpp
@@ -4,3 +4,49 @@ PointCalculate::PointCalculate()
 {
 
 }
+
+Pos PointCalculate::makePos(int x,int y){
+    Pos p;
+    p.x=x;
+    p.y=y;
+    return p;
+}
+
+int PointCalculate::getTilePoints(int color){
+    int tilePoint=0;
+    for(int x=0;x<width;x++){
+        for(int y=0;y<height;y++){
+            if(mainwindow->tile(x,y)->color==color)tilePoint+=tile(x,y)->point;//現在見るべき色のタイルのみを計算に入れる
+        }
+    }
+    return tilePoint;//REDで呼ばれたらREDのタイルポイントを返す、BLUEも同様
+}
+
+int PointCalculate::getAreaPoints(int color){
+    //領域データの設定
+    areaData.clear();
+    checkedData.clear();
+    for(int y=0;y<height;y++){//配列用意の順番的にxからループを始める
+        vector<bool> tmpData;
+        for(int x=0;x<width;x++){
+            tmpData.push_back(false);
+        }
+        areaData.push_back(tmpData);
+        checkedData.push_back(tmpData);
+    }
+
+    for(int y=1;y<height-1;y++){//フィールドの端のマスは領域ポイントを取らないためループから省く
+        for(int x=1;x<width-1;x++){//横に見ていく、全部見たら次の列、という形
+            if(mainWindow->tile(x,y)->color!=color){
+                Pos checkPos=makePos(x,y);
+                checkArea(checkPos);//自タイルが置かれていないなら検証を行う
+            }
+        }
+    }
+}
+
+int PointCalculate::checkArea(){
+
+}
+
+#endif // POINTCALCULATE_H
--- src/PointCalculate.h
+++ src/PointCalculate.h
@@ -1,11 +1,42 @@
 #ifndef POINTCALCULATE_H
 #define POINTCALCULATE_H
 
+#define RED 0
+#define BLUE 1
+
+//#include <vector>
 
 class PointCalculate
 {
 public:
     PointCalculate();
-};
 
+    //color...赤0、青1のようにしてforで処理を繰り返し、コンパクトにする
+    Pos makePos(int x,int y);
+    int getTilePoints(int color);//タイルポイント計算関数
+    int getAreaPoints(int color);//領域ポイント計算関数...データの初期化
+    int checkArea(Pos checkPos);//領域ポイント計算関数...各マスの領域判定
+
+
+    vector<vector<bool>> areaData;
+    vector<vector<bool>> checkedData;
+
+    //以下の変数はのちにmainwindowに集約する、posの定義もmainwindowのほうがいいかも
+
+    int height=10;//仮の縦幅
+    int width=10;//仮の横幅
+
+    struct Pos{
+        int x;
+        int y;
+    };
+
+    struct tile{
+        int x;
+        int y;
+        int color;
+        int points;
+    };
+
+};
 #endif // POINTCALCULATE_H
