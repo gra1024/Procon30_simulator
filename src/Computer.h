b0656e5454b6ead51f4f602f7cabc69ce9c98b36
yueni1024 <31022739+yueni1024@users.noreply.github.com>
2019-09-13 15:08:55 +0900

    [update]computer.h

--- src/Computer.h
+++ src/Computer.h
@@ -2,6 +2,7 @@
 #define COMPUTER_H
 
 #include "Config.h"
+
 #include "ui_MainWindow.h"
 
 using namespace std;
@@ -15,13 +16,25 @@ public:
     vector<vector<Tile>> *tile;
     Teams *teams;
     Field *field;
-    Actions *action;
+    Agent *agent;
+    //Actions *action;
     void startAlgo(int AlgoNumber);
 
 private:
     Ui::MainWindow *uiMainWindow;
     void greedy();
+    void greedy2();
     int eightangle(int angle[8][2],int Ax,int Ay,unsigned int Anumber,int TURN);
+
+    int angle[9][2] = { // x,y
+        { -1, -1},{  0, -1},{  1, -1},
+        { -1,  0},{  0,  0},{  1,  0},
+        { -1,  1},{  0,  1},{  1,  1}
+    };
+    int result[9][2] = {}; // path or fail,point
+    Teams provisionalTeams;
+    int outLange(int x, int y);
+    void copy();
 };
 
 #endif // COMPUTER_H
